a9b080968eb5d3bcd555e10bb29b774d62baacf1
Alex4952 <keyzoo@gmail.com>
2015-11-06 10:07:55 +0900

    Draft

---
+++ Button.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// UI - User Interface 화면을 구성하는 다양한 요소들
+// (글자, 입력박스, 버튼, 스크롤...)
+namespace Unity.UI
+{
+	// 규격문서 (보통 I로 시작) - 버튼을 사용하기 위해 필히 이용해야 하는 규격.
+	interface IButtonEvent
+	{
+		void ButtonClicked(); // 이렇게 만들라고 요청 (접근제어자는 설정하지 않음)
+	}
+
+	// 버튼 클래스 사용법.
+	// Button을 생성 후 해당 객체의 target에
+	// IButtonEvent를 상속받아 기능을 구현한 객체를 넣어주세요.
+	// 버튼이 클릭될 때 해당 기능이 수행됩니다.
+	class Button
+	{
+		public IButtonEvent target;
+
+		public void Click()
+		{
+			// 받은 객체에게 버튼 클릭 명령을 내린다.
+			target.ButtonClicked();
+
+			Console.WriteLine("버튼이 클릭됨.");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.UI;
+
+namespace CSInterface
+{
+	class ButtonEvent : IButtonEvent
+	{
+		// 인터페이스가 시키는 메서드를 만들어 구현한다.
+		public void ButtonClicked()
+		{
+			Console.WriteLine("회원정보를 갱신했습니다.");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Alt+F12 누르면 설명 나옴. (설명의 IButtonEvent에서 또 Alt+F12눌러서 확인함)
+			// 설명중 IButtonEvent을 보면 상속하고 해당 메서드 구현
+			Button myBtn = new Button();
+
+			// 규격문서를 적용한 객체를 전달한다.
+			// static 클래스가 아니면 myBtn.target = this 로 하면 됨.
+			myBtn.target = new ButtonEvent();
+
+			myBtn.Click();
+		}
+
+	}
+}
